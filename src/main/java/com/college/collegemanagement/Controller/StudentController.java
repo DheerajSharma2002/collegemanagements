43e3d75c58af1b1e3f18b68f983a677d0c258b40
dheerajkumar <ds817897@gmail.com>
2024-02-07 13:53:25 +0530

    Create student entity, student repository, student service

--- src/main/java/com/college/collegemanagement/Controller/StudentController.java
+++ src/main/java/com/college/collegemanagement/Controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.college.collegemanagement.Controller;
 
+import com.college.collegemanagement.Entity.Student;
 import com.college.collegemanagement.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/student")
@@ -11,6 +13,16 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @GetMapping()
+    public Flux<Student> getAllStudents(){
+        return studentService.getAllStudent();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Student> getStudentsById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
 
 
 }
