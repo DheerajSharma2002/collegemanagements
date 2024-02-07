2ba227891a56f5d44526a090b5554294631dcbfc
dheerajkumar <ds817897@gmail.com>
2024-02-07 15:34:35 +0530

    Only change student repository

--- src/main/java/com/college/collegemanagement/Repository/StudentRepository.java
+++ src/main/java/com/college/collegemanagement/Repository/StudentRepository.java
@@ -4,8 +4,9 @@ import com.college.collegemanagement.Entity.Student;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface StudentRepository  extends ReactiveMongoRepository<Student, Integer> {
-     Flux<Student> findAllByName(String name);
 }
