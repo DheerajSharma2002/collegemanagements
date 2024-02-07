a377f0de8f1fe1acdc24f42ef8cbd70daf6a8307
dheerajkumar <ds817897@gmail.com>
2024-02-07 15:30:16 +0530

    Create student entity, student repository, student service

--- src/main/java/com/college/collegemanagement/Entity/Student.java
+++ src/main/java/com/college/collegemanagement/Entity/Student.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document("students")
+@Document(value = "students")
 public class Student {
 
     @Id
--- src/main/java/com/college/collegemanagement/Repository/StudentRepository.java
+++ src/main/java/com/college/collegemanagement/Repository/StudentRepository.java
@@ -4,6 +4,8 @@ import com.college.collegemanagement.Entity.Student;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface StudentRepository  extends ReactiveMongoRepository<Student, Integer> {
+     Flux<Student> findAllByName(String name);
 }
